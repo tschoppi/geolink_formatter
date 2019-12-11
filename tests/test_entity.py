0d60b355aa7291448c71ed1f7dc351af581e88ba
Jan Sandberg <jan.sandberg@sitrox.com>
2019-12-11 09:08:10 +0100

    Add property 'filename' to File
    The GEO-Link will emit titles separately from filenames starting with
    version 1.2.0. This should be accounted for within the File class.

--- tests/test_entity.py
+++ tests/test_entity.py
@@ -7,11 +7,12 @@ from geolink_formatter.entity import File, Document
 
 
 def test_file():
-    f = File(title='test.pdf', href='http://my.link.to/file', category='test')
+    f = File(title='File title', href='http://my.link.to/file', category='test', filename='test.pdf')
     assert isinstance(f, File)
-    assert f.title == 'test.pdf'
+    assert f.title == 'File title'
     assert f.href == 'http://my.link.to/file'
     assert f.category == 'test'
+    assert f.filename == 'test.pdf'
 
 
 def test_file_empty():
@@ -20,6 +21,7 @@ def test_file_empty():
     assert f.title is None
     assert f.href is None
     assert f.category is None
+    assert f.filename is None
 
 
 def test_document():
